68a622139717761ff5a93f31becdc6262a84c062
kimmykokonut <kimmykokonut@gmail.com>
2023-10-19 12:01:05 -0700

    initial commit
    Co-authored-by: Christopher Quattrocchi <chrisquattrocchi@gmail.com>
    Co-authored-by: Zuri Gallegos <zurisadailopezmartinez@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+function add(num1, num2) {
+    return num1 + num2;   
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;   
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;   
+}
+
+function divide(num1, num2) {
+    return num1 / num2;   
+}
+
+function handleCalculation(event) {
+    event.preventDefault();
+    const number1 = parseInt(document.querySelector("input#input1").value);
+    const number2 = parseInt(document.querySelector("input#input2").value);
+    const operator = document.querySelector("input[name='operator']:checked").value;
+}
+
+window.addEventListener("load",function() {
+    const form = document.getElementById("calculator");
+    form.addEventListener("submit", handleCalculation);
+});
